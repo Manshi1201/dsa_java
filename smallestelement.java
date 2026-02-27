23c51a145602f65dfbb731b74e83fd6b5d97d553
Manshi1201 <manshi75kumari@gmail.com>
2026-02-27 17:27:44 +0000

    code for smallest element

---
+++ smallestelement.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class smallestelement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int min = arr[0];  // assume first is smallest
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Smallest element is: " + min);
+    }
+}
