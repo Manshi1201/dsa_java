1f1c26a327447962a3f77e4898f22287e4fbda6c
Manshi1201 <manshi75kumari@gmail.com>
2026-03-19 14:07:16 +0000

    DSA: longest consecutive sequence

---
+++ longestconsecutive.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class longestconsecutive {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : arr) {
+            set.add(num);
+        }
+
+        int longest = 0;
+
+        for(int num : set) {
+
+            // check if it's start of sequence
+            if(!set.contains(num - 1)) {
+
+                int current = num;
+                int count = 1;
+
+                while(set.contains(current + 1)) {
+                    current++;
+                    count++;
+                }
+
+                longest = Math.max(longest, count);
+            }
+        }
+
+        System.out.println("Longest consecutive length = " + longest);
+    }
+}
