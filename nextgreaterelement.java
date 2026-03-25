5ddb8043e2293b1a8b5ddb600be9efddba1101b9
Manshi1201 <manshi75kumari@gmail.com>
2026-03-25 17:46:45 +0000

    DSA: Next Greater Element

---
+++ nextgreaterelement.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class nextgreaterelement {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n; i++) {
+
+            int nextGreater = -1;
+
+            for(int j = i + 1; j < n; j++) {
+                if(arr[j] > arr[i]) {
+                    nextGreater = arr[j];
+                    break;
+                }
+            }
+
+            System.out.print(nextGreater + " ");
+        }
+    }
+}
